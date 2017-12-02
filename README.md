d25967d989bbe4cb39da127e14f73f2e7a2451d0
Wael Farhan <wael34218@gmail.com>
2017-12-02 02:55:59 +0200

    Fix data generation and Add requirements

---
+++ README.md
@@ -0,0 +1,15 @@
+# Word Embedding Simple Representation
+
+
+## Introduction
+A simple implementation of Word2Vec using TensorFlow
+
+
+## Installation steps:
+
+    pip install -r requirements.txt
+
+
+## Run:
+
+    python word2vec.py
--- word2vec.py
+++ word2vec.py
@@ -6,12 +6,13 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import arabic_reshaper
 from bidi.algorithm import get_display
+from sklearn.utils import shuffle
 
 
 window_size = 4
-word_min_count = 0
+word_min_count = 6
 skip_gram = 0
-embedding_size = 100
+embedding_size = 75
 epochs = 1
 batch_size = 512
 
@@ -20,8 +21,6 @@ TODO:
     - Negative Sampling
     - SkipGram vs CBOW
     - Agparse
-    - word_min_count
-    - Number Normalization
 '''
 
 
@@ -34,14 +33,15 @@ def clean(line):
 def one_hot(obj, size):
     temp = np.zeros(size * len(obj))
     for i, token in enumerate(obj):
-        temp[i*vocab_size + token] = 1
+        if token > -1:
+            temp[i*vocab_size + token] = 1
     return temp
 
 sentences = []
 
 print("Read corpus")
 word_count = defaultdict(lambda: 0)
-with open("corpus_sample.txt") as f:
+with open("corpus.txt") as f:
     for line in f:
         sentence = clean(line).split()
         sentences.append(sentence)
@@ -51,62 +51,71 @@ with open("corpus_sample.txt") as f:
 print("Tokenize")
 tokens = sorted(word_count, key=word_count.get, reverse=True)
 
-tok2int = {}
-int2tok = {}
+tok2int = {"PAD": -1}
+int2tok = {-1: "PAD"}
 for i, word in enumerate(tokens):
     if word_count[word] > word_min_count:
         tok2int[word] = i
         int2tok[i] = word
 
+
 vocab_size = len(int2tok)
+print("Total Vocabs:", vocab_size)
+
 train_x = []
 train_y = []
 
 print("Prepare training data")
 for sentence in sentences:
-    for start_index in range(len(sentence) - window_size):
-        context = sentence[start_index:min(start_index + window_size, len(sentence) - 1)]
-        for i in range(len(context)):
-            train_x.append(one_hot([tok2int[context[i]]], vocab_size))
-            train_y.append(one_hot([tok2int[w] for j, w in enumerate(context) if j != i],
-                                   vocab_size))
+    sentence = [w for w in sentence if word_count[w] > word_min_count]
+    for word_index in range(len(sentence)):
+        context = ["PAD"] * max((window_size - word_index), 0)
+        context += sentence[max(word_index - window_size, 0):word_index]
+        context += sentence[word_index + 1:min(word_index + window_size + 1, len(sentence))]
+        context += ["PAD"] * (window_size * 2 - len(context))
+        word = sentence[word_index]
+        train_x.append(one_hot([tok2int[word]], vocab_size))
+        train_y.append(one_hot([tok2int[w] for w in context], vocab_size))
+
+train_x, train_y = shuffle(train_x, train_y)
 
 # Network
 print("Train NN")
-output_length = vocab_size * (window_size - 1)
-x = tf.placeholder(tf.float32, shape=(None, vocab_size))
-y = tf.placeholder(tf.float32, shape=(None, output_length))
+output_length = vocab_size * (window_size * 2)
+x = tf.placeholder(tf.float64, shape=(None, vocab_size))
+y = tf.placeholder(tf.float64, shape=(None, output_length))
 
-std = (2 / (vocab_size + embedding_size)) ** 0.5
-W1 = tf.Variable(tf.random_normal([vocab_size, embedding_size], mean=0, stddev=std))
-b1 = tf.Variable(tf.zeros([embedding_size]))
+stddev = (2 / (vocab_size + embedding_size)) ** 0.5
+W1 = tf.Variable(tf.random_normal([vocab_size, embedding_size], stddev=stddev, dtype=tf.float64))
+b1 = tf.Variable(tf.zeros([embedding_size], dtype=tf.float64))
 Y1 = tf.add(tf.matmul(x, W1), b1)
 
-
-std = (2 / (output_length + embedding_size)) ** 0.5
-W2 = tf.Variable(tf.random_normal([embedding_size, output_length], mean=0, stddev=std))
-b2 = tf.Variable(tf.zeros([output_length]))
+stddev = (2 / (output_length + embedding_size)) ** 0.5
+W2 = tf.Variable(tf.random_normal([embedding_size, output_length], stddev=stddev, dtype=tf.float64))
+b2 = tf.Variable(tf.zeros([output_length], dtype=tf.float64))
 pred = tf.nn.softmax(tf.add(tf.matmul(Y1, W2), b2))
 
 loss = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred), reduction_indices=[1]))
 
-optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
+    total_batch = int(len(train_x)/batch_size)
+    print("Total batches:", total_batch)
     for iteration in range(epochs):
-        total_batch = int(len(train_x)/batch_size)
+        total_loss = 0
         for b in range(total_batch):
             start = b * batch_size
             end = start + batch_size
-            sess.run(optimizer, feed_dict={x: train_x[start:end], y: train_y[start:end]})
-            if b % 10 == 0:
-                print("    Loss", b, "of", total_batch,
-                      sess.run(loss, feed_dict={x: train_x[start:end], y: train_y[start:end]}))
-        print("Iter", iteration, "loss:",
-              sess.run(loss, feed_dict={x: train_x[start:end], y: train_y[start:end]}))
+            loss_value, _ = sess.run([loss, optimizer],
+                                     feed_dict={x: train_x[start:end], y: train_y[start:end]})
+            total_loss += loss_value / batch_size
+            if (b) % 10 == 0 and b > 0:
+                print("   batch loss", b, "is", loss_value)
+        print("Total Loss of iter", iteration, "is", total_loss)
     print("Tuning Completed!")
     vectors = sess.run(W1 + b1)
 
@@ -114,11 +123,12 @@ print("TSNE")
 vectors = TSNE(n_components=2).fit_transform(vectors)
 
 print("Plot")
-fig = plt.figure(figsize=(18, 18), dpi=280)
+fig = plt.figure(figsize=(14, 14))
 plt.plot()
 ax = fig.add_subplot(111)
-for i in range(50):
+for i in range(100):
     print(int2tok[i], vectors[i][0], vectors[i][1])
     plt.plot(vectors[i][0], vectors[i][1])
     ax.annotate(get_display(arabic_reshaper.reshape(int2tok[i])), xy=vectors[i], fontsize=8)
-plt.show()
+
+plt.savefig('word_plot.png', bbox_inches='tight')
